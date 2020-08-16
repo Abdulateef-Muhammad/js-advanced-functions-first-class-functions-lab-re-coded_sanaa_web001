2dd91549f9602a058ed5152623e68733b7e7fc55
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-16 11:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return [drivers[0], drivers[1]];
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return [drivers[drivers.length -2], drivers[drivers.length-1]];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(number) {
+  return function(fare) {
+    return number * fare;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+fareDoubler(2);
+
